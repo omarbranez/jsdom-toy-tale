b1276fc62336ef10484a7e6308119ccbb3dc27c0
Omar <omar.branez@gmail.com>
2021-08-09 13:22:24 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-let addToy = false;
+let toyCollection = document.querySelector("#toy-collection")
 
 document.addEventListener("DOMContentLoaded", () => {
+  let addToy = false;
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
@@ -8,8 +9,92 @@ document.addEventListener("DOMContentLoaded", () => {
     addToy = !addToy;
     if (addToy) {
       toyFormContainer.style.display = "block";
-    } else {
-      toyFormContainer.style.display = "none";
+      toyFormContainer.addEventListener("submit", e => {
+        e.preventDefault()
+        addNewToy(e.target)
+      }) 
+      toyFormContainer.reset();
+      } else {
+        toyFormContainer.style.display = "none";
     }
-  });
+  })
+})
+
+
+function getToys() {
+  return fetch('http://localhost:3000/toys') // dont forget return, in order to use it later!!
+    .then(res => res.json())
+};
+
+function displayToys(toy) {  
+    
+  let toyName = document.createElement("h2")
+  toyName.innerText = toy.name
+
+  let toyImage = document.createElement("img")
+  toyImage.src = toy.image
+  toyImage.setAttribute("class", "toy-avatar")
+    
+  let toyLikes = document.createElement("p")
+  toyLikes.innerText = `${toy.likes} Likes`
+
+  let likeButton = document.createElement("button")
+  likeButton.setAttribute("class", "like-btn")
+  likeButton.setAttribute("id", toy.id)
+  likeButton.innerText = "Like"
+  likeButton.addEventListener("click", (e) => {
+    addLike(e)
+  })
+    
+  let newToy = document.createElement("div")
+  newToy.setAttribute = ("class", "card")
+  newToy.append(toyName, toyImage, toyLikes, likeButton)
+  toyCollection.append(newToy)
+};
+
+function addLike(e) {
+  e.preventDefault()
+  let newLikes = parseInt(e.target.previousElementSibling.innerText) + 1
+  return fetch(`http://localhost:3000/toys/${e.target.id}`, {
+    method: "PATCH",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      "likes": newLikes
+    })
+  })
+  .then(res => res.json())
+  .then(newObjectLikes => {
+    e.target.previousElementSibling.innerText = `${newObjectLikes.likes} Likes`;
+  
+  })
+}
+
+function addNewToy(toy_form) {
+  fetch("http://localhost:3000/toys", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      "name": toy_form.name.value,
+      "image": toy_form.image.value,
+      "likes" : 0
+    })
+  })
+  .then(res => res.json())
+  .then(toy_object => {
+    displayToys(toy_object)
+    document.querySelector(".add-toy-form").reset()
+    // otherwise the data stays on the form
+  })
+}
+
+getToys().then(toys => {
+  toys.forEach(toy => {
+    displayToys(toy)
+  })
 });
